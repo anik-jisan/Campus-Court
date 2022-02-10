4e73e51e7fa747596571724e74549c814df576e3
anik-jisan <anik08838@gmail.com>
2022-02-10 19:18:24 +0600

    top banner

---
+++ js/script.js
@@ -0,0 +1,11 @@
+//active navbar
+
+let nav = document.querySelector(".navigation-wrap");
+window.onscroll = function () {
+    if(document.documentElement.scrollTop > 20){
+        nav.classList.add("scroll-on");
+    }else{
+        nav.classList.remove("scroll-on");
+
+    }
+}
